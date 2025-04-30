782816eb4d8d07552474174d35a60950ab4a7369
TrynaGITEducated <booysenhs1998@gmail.com>
2025-04-30 19:47:22 +0200

    initial-commit

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.10/typed.min.js
+document.addEventListener("DOMContentLoaded", () => {
+    new Typed("#typing-text", {
+      strings: ["Web Development", "UI/UX Design", "ML/AI", "Java Development", "Just..Tech"],
+      typeSpeed: 70,
+      backSpeed: 50,
+      backDelay: 2000,
+      loop: true,
+      showCursor: true,
+      cursorChar: "|"
+    });
+  });
+  
